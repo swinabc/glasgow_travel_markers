1d211fba56cfbc2335c93adb917fc2806ff11faa
ben <bswin@hotmail.co.uk>
2019-04-04 03:58:58 +0000

    Maps added and linked to buttons both mobile and desktop

--- js/JQuery.js
+++ js/JQuery.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
-    $("#test1,#test2,#test3,#test4").hide();
-    $("#test0").click(function(){
-        $("#test1,#test2,#test3,#test4").slideToggle();
+    
+    $("#glasgowMobile,#edinburgeMobile,#aberdeenMobile,#dundeeMobile,#map2,#map3,#map4,#map5").hide();
+    
+    $("#locations").click(function(){
+        $("#glasgowMobile,#edinburgeMobile,#aberdeenMobile,#dundeeMobile").slideToggle();
+    });
+    $("#glasgowMobile,.glasgow").click(function(){
+        $("#map2").slideToggle();
+        $("#map1,#map3,#map4,#map5").hide();
+    });
+    $("#edinburgeMobile,.edinburge").click(function(){
+        $("#map3").slideToggle();
+        $("#map1,#map2,#map4,#map5").hide();
+    });
+    $("#dundeeMobile,.dundee").click(function(){
+        $("#map4").slideToggle();
+        $("#map1,#map3,#map2,#map5").hide();
+    });
+    $("#aberdeenMobile,.aberdeen").click(function(){
+        $("#map5").slideToggle();
+        $("#map1,#map3,#map4,#map2").hide();
     });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,119 @@
+// default settings -----------------------------------------     
     function initMap() {
              
              var options = {
                zoom:6,
                center:{lat:57.068574,lng:-4.349972}
              };
+             
+             var glasgowOptions = {
+                 zoom:13,
+                 center:{lat:55.864125,lng:-4.254217}
+             };
+             
+             var edinburgeOptions = {
+                 zoom:13,
+                 center:{lat:55.953414,lng:-3.188287}
+             };
+             
+             var dundeeOptions = {
+                 zoom:13,
+                 center:{lat:56.4620,lng:-2.9707}
+             };
+             
+             var aberdeenOptions = {
+                 zoom:13,
+                 center:{lat:57.1497,lng:-2.0943}
+             };
+             
+// map front page api --------------------------------------------------------
+             var map = new google.maps.Map(document.getElementById('map1'), options);
 
-             var map = new google.maps.Map(document.getElementById('map'), options);
-
-
-            //  var marker = new google.maps.Marker({
-            //   position:{lat:55.864125,lng:-4.254217},/*{lat:55.953414,lng:-3.188287},*/
-            //   map:map
-            //  });
-               
-            //   var infoWindow = new google.maps.InfoWindow({
-            //       content:'<h3>Glasgow</h3>'
-            //   });
-               
-            //   marker.addListener('click', function(){
-            //       infoWindow.open(map, marker);
-            //   });
-            addMarker({lat:55.864125,lng:-4.254217});
-            addMarker({lat:55.953414,lng:-3.188287}); 
-            addMarker({lat:56.4620,lng:-2.9707}); 
-            addMarker({lat:57.1497,lng:-2.0943}); 
+            addMarker({coords:{lat:55.864125,lng:-4.254217},
+                content:'<h3>Glasgow</h3>'
+            });
+            addMarker({coords:{lat:55.953414,lng:-3.188287},
+                content:'<h3>Edinburge</h3>'
+            }); 
+            addMarker({coords:{lat:56.4620,lng:-2.9707},
+                content:'<h3>Dundee</h3>'
+            }); 
+            addMarker({coords:{lat:57.1497,lng:-2.0943},
+                content:'<h3>Aberdeen</h3>'
+            }); 
             
-            function addMarker(coords){
+            function addMarker(props){
                 var marker = new google.maps.Marker({
-                position:coords,
+                position:props.coords,
                 map:map
                     });
-                 }
-             }
-/*{lat:55.864125,lng:-4.254217},{lat:55.953414,lng:-3.188287},*/
+                    if(props.content){
+                        var infoWindow = new google.maps.InfoWindow({
+                            content:props.content
+                        });
+                        marker.addListener('click', function() {
+                            infoWindow.open(map, marker);
+                        });
+                    }
+                 } 
+// end of front map options ----------------------------------------------------------
+
+// Glasgow map ------------------------------------------------------------------------------------------------------------------------------------------------------------
+             var map2 = new google.maps.Map(document.getElementById('map2'), glasgowOptions);
+
+                addMarker2({coords2:{lat:55.864125,lng:-4.254217}});
+
+            
+                function addMarker2(props){
+                    var marker = new google.maps.Marker({
+                    position:props.coords2,
+                    map:map2
+                        });
+                    }
+// Glasgow map end ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Edinburge map Start -------------------------------------------------------------------------------------------------------------------------------------------------------- 
+             var map3 = new google.maps.Map(document.getElementById('map3'), edinburgeOptions);
+
+                addMarker3({coords3:{lat:55.953414,lng:-3.188287}});
+
+            
+                function addMarker3(props){
+                    var marker = new google.maps.Marker({
+                    position:props.coords3,
+                    map:map3
+                        });
+                    }
+// Edinburge map end -------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
+// Dundee map Start -------------------------------------------------------------------------------------------------------------------------------------------------------- 
+             var map4 = new google.maps.Map(document.getElementById('map4'), dundeeOptions);
+
+                addMarker4({coords4:{lat:56.4620,lng:-2.9707}});
+
+            
+                function addMarker4(props){
+                    var marker = new google.maps.Marker({
+                    position:props.coords4,
+                    map:map4
+                        });
+                    }
+// Dundee map end -------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
+// Aberdeen map Start --------------------------------------------------------------------------------------------------------------------------------------------------------
+             var map5 = new google.maps.Map(document.getElementById('map5'), aberdeenOptions);
+
+                addMarker5({coords5:{lat:57.1497,lng:-2.0943}});
+
+            
+                function addMarker5(props){
+                    var marker = new google.maps.Marker({
+                    position:props.coords5,
+                    map:map5
+                        });
+                    }
+// Aberdeen map end -------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
+             } // end of init function ----------------------------------------------------------
 
            
-console.log("hello");
